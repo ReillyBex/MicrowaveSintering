5dd1a687ba2d305c296a2633a036cc3874eb16cd
ReillyBex <71298975+ReillyBex@users.noreply.github.com>
2023-02-22 21:05:01 -0700

    removed unneeded files

--- UpdateRepo.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# This script can be used to commit changes to git repo and push to
-# remote repo all in one command.
-TODAY=$(date)
-HOST=$(hostname)
-git add .
-git commit -m "Changes committed: $TODAY from $HOST"
-git push
-git pull
